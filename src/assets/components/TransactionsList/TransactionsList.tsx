7ccf74aae78c220b1774df837e26b411c5e550ff
John Doe <johndoe@example.com>
2023-06-08 17:30:30 +0300

    add cardBalance and delete files

--- src/assets/components/TransactionsList/TransactionsList.tsx
+++ src/assets/components/TransactionsList/TransactionsList.tsx
@@ -1,9 +1,17 @@
-// import styles from './style.module.scss'
+import styles from './style.module.scss'
+import CardBalance from "./CardBalance/CardBalance.tsx";
+import {Col, Row} from "react-bootstrap";
+
 const TransactionsList = () => {
     return (
-        <>
-            
-        </>
+        <div className={styles.cardSection}>
+            <Row>
+                <Col xs={12} md={4}>
+                    <CardBalance balance={17.3} available={1000}/>
+                </Col>
+
+            </Row>
+        </div>
     );
 };
 
